859ad6fa8327cda40b5e5340d120b984e0dbd0bd
tIoImIcIaItI <djkinggithub@outlook.com>
2018-05-05 17:00:14 -0600

    feat: Enhance hour table for screen readers

--- src/js/hours.js
+++ src/js/hours.js
@@ -6,16 +6,35 @@ import moment from 'moment';
 const hoursHtml = (operatingHours) => {
 	const regex = /(\d{1,2})\:(\d\d)\s(am|pm)/g;
 
-	return (operatingHours || '').replace(regex, (match) => {
-		const date = moment(match, 'h:mm A').format('HH:mm');
-		return `<time datetime="${date}">${match}</time>`;
-	});
+	return (operatingHours || '').
+		replace(regex, (match) => {
+			const date = moment(match, 'h:mm A').format('HH:mm');
+			return `<time datetime="${date}">${match}</time>`; }).
+		replace(' - ', '<span class="sr-only"> through </span><span aria-hidden="true"> - </span>');
+};
+
+const headerRow = () => {
+	const row = document.createElement('tr');
+	row.className = 'sr-only';
+
+	const dayCol = document.createElement('th');
+	dayCol.innerHTML = 'Day';
+	row.appendChild(dayCol);
+
+	const timeCol = document.createElement('th');
+	timeCol.innerHTML = 'Hours of Operation';
+	row.appendChild(timeCol);
+
+	return row;
 };
 
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
 export const fillHoursHtml = (document, root, operatingHours) => {
+
+	root.appendChild(headerRow());
+
 	for (let key in operatingHours) {
 		if (operatingHours.hasOwnProperty(key)) {
 			const row = document.createElement('tr');
@@ -31,4 +50,5 @@ export const fillHoursHtml = (document, root, operatingHours) => {
 			root.appendChild(row);
 		}
 	}
+
 };
